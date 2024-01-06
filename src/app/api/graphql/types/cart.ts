7d91e9b97f32f3cefa5408b69f9127845d970348
Arthur-Gab <arthurgabriel933@gmail.com>
2024-01-06 13:40:02 -0300

    bug: Ao adicionar a opcao include na definicao do prismaObject a propriedade _count não está sendo passada para o parametro query no queryField('getCartById')

--- src/app/api/graphql/types/cart.ts
+++ src/app/api/graphql/types/cart.ts
@@ -1,6 +1,10 @@
 import { PrismaClientKnownRequestError } from '@prisma/client/runtime/library';
 import { builder } from '../builder';
 import { GraphQLError } from 'graphql';
+import { Money } from './money';
+import { Cart } from '@prisma/client';
+import { includeForRefMap } from '@pothos/plugin-prisma/dts/util/datamodel';
+import { prismaModelName } from '@pothos/plugin-prisma';
 
 builder.mutationField('createCart', (t) =>
 	t.prismaField({
@@ -43,9 +47,12 @@ builder.queryField('getCartByID', (t) =>
 		},
 		type: 'Cart',
 		nullable: true,
-		resolve: async (query, _, { input: { cartId } }, { db }) => {
+		resolve: async (query, __, { input: { cartId } }, { db }) => {
 			try {
+				console.log(query);
+
 				const findedCart = await db.cart.findUniqueOrThrow({
+					...query,
 					where: {
 						id: cartId,
 					},
@@ -79,16 +86,41 @@ builder.queryField('getCartByID', (t) =>
 builder.prismaObject('Cart', {
 	description:
 		'Carrinho de compras, contendo seus items, o total de itens e o subtotal da compra',
+	// Add include to query on resolvers and type parent with these types
+	include: { items: true, _count: true },
 	fields: (t) => ({
-		id: t.exposeID('id'),
-		totalItems: t.int({
-			resolve: (parent, __, { db }): Promise<number> => {
-				console.log(parent);
-
-				// On Progress
-				return Promise.resolve(0);
+		id: t.exposeID('id', {
+			description: 'Identificador unico do Carrinho',
+		}),
+		items: t.prismaField({
+			description: 'Items adicionados ao carrinho.',
+			type: ['CartItem'],
+			nullable: {
+				list: false,
+				items: true,
+			},
+			resolve: async (_, parent) => {
+				return parent.items;
 			},
 		}),
+		totalItems: t.int({
+			description: 'Quantidade Total de Itens adicionado ao carrinho',
+			resolve: async (parent) => {
+				console.log(parent);
+				//@ts-ignore
+				// return parent._count.items;
+
+				return 0;
+			},
+		}),
+		// subTotal: t.field({
+		// 	description: 'Subtotal da compra dos itens adicionados ao carrinho',
+		// 	type: Money,
+		// 	nullable: true,
+		// 	resolve: async (parent) => {
+		// 		return parent.items.reduce();
+		// 	},
+		// }),
 	}),
 });
 
